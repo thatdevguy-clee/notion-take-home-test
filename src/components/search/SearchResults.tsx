4110d391c20265a211e4568260e87b91ca40b9a3
gogo125 <gogo125@hotmail.com>
2023-07-14 11:18:49 -0700

    minor code and spacing cleanup

--- src/components/search/SearchResults.tsx
+++ src/components/search/SearchResults.tsx
@@ -1,7 +1,7 @@
 
 import ResultItem from './ResultItem';
 import { useEffect, useState } from 'react';
-import Papa from "papaparse";
+import { parse, ParseResult } from "papaparse";
 
 type SearchDataProps = {
   UNITID: number,
@@ -11,35 +11,25 @@ type SearchDataProps = {
   INSTURL: string
 }
 
-type Values = {
-  data: SearchDataProps[]
-}
-
 type SearchResultsProps = {
   searchQuery: string
 }
 
+const csvUrl = 'https://gist.githubusercontent.com/simonlast/d5a86ba0c82e1b0d9f6e3d2581b95755/raw/f608b9b896dd3339df13dae317998d5f24c00a50/edu-scorecard.csv';
+
 const SearchResults = ({ searchQuery }: SearchResultsProps) => {
-  const [searchData, setSearchData] = useState<any>();
-  const [loading, setLoading] = useState(false);
+  const [searchData, setSearchData] = useState<SearchDataProps[]>();
 
   useEffect(() => {
-    setLoading(true)
-    Papa.parse('https://gist.githubusercontent.com/simonlast/d5a86ba0c82e1b0d9f6e3d2581b95755/raw/f608b9b896dd3339df13dae317998d5f24c00a50/edu-scorecard.csv', {
+    parse(csvUrl, {
       download: true,
       header: true,
       skipEmptyLines: true,
-      complete: results => {
+      complete: (results: ParseResult<SearchDataProps>) => {
         setSearchData(results.data);
-        setLoading(false);
       }
     });
-
   }, []);
-
-  // useEffect(() => {
-  //   console.log('values', searchData);
-  // }, [searchData]);
 
   return (
     <section className="results grid gap-x-6 gap-y-12 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
